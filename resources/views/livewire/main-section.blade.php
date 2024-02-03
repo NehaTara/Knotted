3cad3624f73b96cc6180e01ff628f551ae1e7798
NehaTara <nehatara102@gmail.com>
2024-02-03 12:21:20 +0530

    commit

---
+++ resources/views/livewire/main-section.blade.php
@@ -0,0 +1,79 @@
+<div>
+    
+
+<div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+            <div class="max-w-7xl mx-auto p-6 lg:p-8">
+              
+
+            <section class="bg-center bg-no-repeat bg-cover bg-[url('https://i.pinimg.com/564x/5d/1c/5e/5d1c5e85579c280ea2e198540ff3692e.jpg')] bg-gray-700 bg-blend-multiply">
+                <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+                    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Crafting Your Dream Celebration</h1>
+                    <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Knotted, we provide a hassle-free wedding planning experience, allowing couples to effortlessly organize their special day through our platform.</p>
+                    <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+                        <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                            Get started
+                            <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a>
+                        <a href="#" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                            Learn more
+                        </a>  
+                    </div>
+                </div>
+            </section>
+
+
+            <div class="flex items-center justify-center h-28">
+              <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+                Gallery
+              </h1>
+            </div>
+
+          <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+              <div class="grid gap-4">
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg object-cover" src="https://i.pinimg.com/564x/6a/b8/f4/6ab8f4e0d42b0d0188def6451704d53b.jpg" alt="">
+                  </div>
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg object-fill" src="https://i.pinimg.com/564x/62/72/94/6272940b4b88587676e3dc88b55a32f7.jpg" alt="">
+                  </div>
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/564x/ac/70/b2/ac70b2089f2f0ad3368eaec5c7145a0f.jpg" alt="">
+                  </div>
+              </div>
+              <div class="grid gap-4">
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/564x/33/b5/05/33b5052ee1e2c42bdb4ce6e693de7804.jpg" alt="">
+                  </div>
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/736x/3c/b5/50/3cb55097ba5e22fd1cd52895cb9f802f.jpg" alt="">
+                  </div>
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg" src="https://img.freepik.com/free-photo/huge-decorative-circle-made-willow-greenery-pale-orange-roses-with-burning-candles_8353-10644.jpg" alt="">
+                  </div>
+              </div>
+              <div class="grid gap-4">
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/564x/91/21/34/91213465b94b2180a1af5011e64a6475.jpg" alt="">
+                  </div>
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/564x/23/ae/64/23ae647608054ff958686cdf861ba61d.jpg" alt="">
+                  </div>
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/564x/b5/4d/2a/b54d2a8629479b2a58c457f83460aa60.jpg" alt="">
+                  </div>
+              </div>
+              <div class="grid gap-4">
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/736x/09/42/14/0942149d7142c6ac9a54df5736670e1b.jpg" alt="">
+                  </div>
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/564x/4f/34/6d/4f346d801cb110390ebf2355b754cc75.jpg" alt="">
+                  </div>
+                  <div>
+                      <img class="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/564x/88/c8/14/88c81411628c0d18ddd2e8da9eaf2f7f.jpg" alt="">
+                  </div>
+              </div>
+          </div>
+</div>
